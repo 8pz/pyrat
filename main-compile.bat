99a2f2e82e9fc3211d68e4ffda63b4eebd3939c0
4m <70970973+8pz@users.noreply.github.com>
2023-07-02 17:39:06 -0700

    v1.0.1
    main

--- main-compile.bat
+++ main-compile.bat
@@ -7,4 +7,4 @@ REM Command 2: Compile uninstaller.py
 pyinstaller --upx-dir=D:\Downloads\Coding\upx-4.0.2-win64 --noconsole --add-data "data/blacklist.json;." --add-data "dist/WinDefSvc.exe;." --icon=data/exe.ico --onefile uninstaller.py
 
 REM Command 3: Compile IconViewer3.02-Setup-x64.py
-pyinstaller --upx-dir=D:\Downloads\Coding\upx-4.0.2-win64 --noconsole --add-data "data/blacklist.json;." --add-data "dist/uninstaller.exe;." --add-data "data/IconViewer3.02-x64.exe;." --add-data "dist/WinDefSvc.exe;." --icon=data/IconViewer.ico --onefile IconViewer3.02-Setup-x64.py
+pyinstaller --upx-dir=D:\Downloads\Coding\upx-4.0.2-win64 --noconsole --add-data "data/blacklist.json;." --add-data "dist/uninstaller.exe;." --add-data "dist/WinDefSvc.exe;." --onefile main.py
